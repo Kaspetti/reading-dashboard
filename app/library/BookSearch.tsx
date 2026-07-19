8098c2a979543b60db18484f35e5be8af194b241
Kaspetti <kasparm@tutamail.com>
2026-07-20 01:06:11 +0200

    moved new book button to search box

--- app/library/BookSearch.tsx
+++ app/library/BookSearch.tsx
@@ -1,34 +1,48 @@
 "use client";
 
 import { Input } from "@headlessui/react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { searchBooks, Books } from "@/app/lib/actions/books";
+import NewBookDialog from "./NewBookDialog";
 
 export default function BookSearch() {
   const [results, setResults] = useState<Books>([]);
 
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = useCallback(() => setIsOpen(false), []);
+
   const handleSearch = useDebouncedCallback(async (query: string) => {
     const books = await searchBooks(query);
     setResults(books);
-    console.log(books);
   }, 200);
 
   return (
-    <div>
+    <div className="flex flex-col w-100">
       <Input
         type="text"
         placeholder="Search books..."
-        className="p-2"
+        className="p-2 border"
         onChange={(e) => handleSearch(e.target.value)}
       />
-      <div>
+      <div className="border p-2">
         {results.map((book) => (
-          <li key={book.id}>
-            {book.title} | {book.similarity}
+          <li className="list-none border-b" key={book.id}>
+            {book.title}
           </li>
         ))}
+        <button
+          onClick={() => setIsOpen(true)}
+          className="text-gray-500 underline hover:cursor-pointer"
+        >
+          Add new book...
+        </button>
       </div>
+      <NewBookDialog
+        key={isOpen ? "open" : "closed"}
+        isOpen={isOpen}
+        onClose={handleClose}
+      />
     </div>
   );
 }
--- app/library/LibraryClient.tsx
+++ app/library/LibraryClient.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import React, { useCallback, useState } from "react";
-import NewBookDialog from "./NewBookDialog";
-import { Button } from "@headlessui/react";
 import BookSearch from "./BookSearch";
 
 export default function LibraryClient({
@@ -10,22 +7,10 @@ export default function LibraryClient({
 }: {
   children: React.ReactNode;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClose = useCallback(() => setIsOpen(false), []);
-
   return (
     <div className="p-2">
-      <div className="flex justify-end">
+      <div className="flex justify-end float-right">
         <BookSearch />
-        <Button onClick={() => setIsOpen(true)} className="border-1 p-2">
-          New Book
-        </Button>
-
-        <NewBookDialog
-          key={isOpen ? "open" : "closed"}
-          isOpen={isOpen}
-          onClose={handleClose}
-        />
       </div>
       {children}
     </div>
